9cd604b75fadbc799c848522600da337fb6c4763
VITU-PC\victo <victorhbl12@hotmail.com>
2018-03-27 23:25:30 -0300

    Projeto final de RV/RV
    Usando marcador(nota de cinco reais) gera um plano e permite iniciar uma animacao ao apertar em um botao no marcador.

---
+++ Assets/Scripts/vbEvent.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class vbEvent : MonoBehaviour, IVirtualButtonEventHandler {
+
+	private GameObject btnObject;
+	private GameObject zombie;
+
+	public AudioClip audio;
+	public AudioSource audioSource;
+
+	void Start () {
+		btnObject = GameObject.Find ("VirtualButton");
+		btnObject.GetComponent<VirtualButtonBehaviour> ().RegisterEventHandler (this);
+
+		zombie = GameObject.Find ("zombie");
+
+		audioSource.clip = audio;
+
+	}
+
+	public void OnButtonPressed(VirtualButtonBehaviour vb){
+		zombie.GetComponent<Animation> ().Play ();
+		audioSource.Play ();
+	}
+
+	public void OnButtonReleased(VirtualButtonBehaviour vb){
+		zombie.GetComponent<Animation> ().Stop ();
+		audioSource.Stop ();
+	}
+}
+	
